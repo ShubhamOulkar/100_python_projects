b8c021dee1538c352b9f8ae0a594a9f7f9f73b38
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-27 19:22:09 +0530

    added readme file

---
+++ Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator\n")
+
+ur_name = input("What is your name ?\n")
+pet_name = input("What is your pet name ?\n")
+city_name = input("Where do you born?\n")
+
+print(f"Name Generated : {pet_name+' '+ur_name+' '+city_name}")
