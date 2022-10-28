242a701fc8285114177b46970e518622a172efe3
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 08:03:22 +0530

    added readme file

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -3,6 +3,14 @@
 # print("sum =", int(a[0])+int(a[1]))
 # print(type(a))
 
-
-
-
+print("Welcome to the tip Calculator")
+bill = float(input("What was the total bill? $"))
+percent = float(input("What percentage tip would you like to give? "))
+people = float(input("How many people to split the bill?"))
+bill_tip = bill + (bill*percent*0.01)
+pay = round(bill_tip / people,2)
+print("-----------------------------------------------------------------------------------------------")
+print(f"Each person should pay: ${pay}")
+print("-----------------------------------------------------------------------------------------------")
+print(f"Bill payed by each person is ${round(bill / people,2)}")
+print(f"Tip payed by each person is ${round((bill*percent*0.01)/people,2)}")
