982b2c04cabfed75fcf4b59637cae23620e466e7
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 07:29:52 +0530

    added readme file

---
+++ Life_in_weeks.py
@@ -0,0 +1,4 @@
+age = int(input("Input your age :"))
+print(f"days left in your life: {(90*365-age*365)}")
+print(f"weeks left in your life: {(90*52-age*52)}")
+print(f"days left in your life: {(90*12-age*12)}")
