eb22a680b8b6b82eaccee9526511fc85fa76f31a
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 07:06:30 +0530

    added readme file

---
+++ BMI_calculator.py
@@ -0,0 +1,3 @@
+height = input("Input your height in meters: ")
+weight = input("Input your weight in kilogram: ")
+print("BMI = ", round(float(weight)/(float(height))**2, 2))
---
+++ Tip_Calculator.py
@@ -0,0 +1,8 @@
+# number is input() as string in python
+# a = input("Type two digit number: ")
+# print("sum =", int(a[0])+int(a[1]))
+# print(type(a))
+
+
+
+
