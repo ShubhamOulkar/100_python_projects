a8006a7ce5bbaf74a26db9508d36e9fc1c8c1bee
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 15:33:44 +0530

    added readme file

--- Love_Calculator.py
+++ Love_Calculator.py
@@ -5,7 +5,7 @@ name = input("Type Your name: \n").lower()
 partner = input("Type your partner name: \n").lower()
 true = []
 love = []
-string = ["t","r","u","e","l","o","v","e"]
+string = ["t", "r", "u", "e", "l", "o", "v", "e"]
 for i in string[0:4]:
     true.append(name.count(i))
     true.append(partner.count(i))
@@ -14,4 +14,11 @@ for q in string[4:8]:
     love.append(partner.count(q))
 love_score = int(str(sum(true))+str(sum(love)))
 
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif 50 >= love_score >= 40:
+    print(f"Your score is {love_score}, you are alright together.")
+else :
+    print(f"Your score is {love_score}.")
 
+print("\n---------- shubhuoulkar@gmail.com ----------")
