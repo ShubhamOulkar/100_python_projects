428039b10651c6ed4a0b11d97184404cd702a831
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 18:58:44 +0530

    added readme file

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -36,12 +36,12 @@ scissors = '''
 '''
 art = [rock, paper, scissors]
 you = int(input("\n Input 1 for Rock \n Input 2 for Paper\n Input 3 for Scissors\n"))
-if you >= 3 or you < 0:
+if you > 3 or you < 0:
     print("You lose! Enter valid number")
 else:
-    print(art[you])
+    print(art[you-1])
     computer = random.randint(1, 3)
-    print(art[computer])
+    print(art[computer-1])
     if you == 1 and computer == 3:
         print("You win!")
     elif you == 3 and computer == 2:
