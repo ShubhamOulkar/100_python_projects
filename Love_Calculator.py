c949fedc8071038a023bb419e8809aeb6bb1cee0
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 15:28:40 +0530

    added readme file

--- Love_Calculator.py
+++ Love_Calculator.py
@@ -1,5 +1,17 @@
 print("""----------------------------------------------------------
              Love Calculator 2022
 ----------------------------------------------------------""")
-year = int(input("Which year do you want to check?\n"))
-print("----------------------------------------------------------")
+name = input("Type Your name: \n").lower()
+partner = input("Type your partner name: \n").lower()
+true = []
+love = []
+string = ["t","r","u","e","l","o","v","e"]
+for i in string[0:4]:
+    true.append(name.count(i))
+    true.append(partner.count(i))
+for q in string[4:8]:
+    love.append(name.count(q))
+    love.append(partner.count(q))
+love_score = int(str(sum(true))+str(sum(love)))
+
+
