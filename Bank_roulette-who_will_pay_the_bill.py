c5f6dc6cfe9af0184c2b149a36efbc4af15ed92d
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-28 17:35:13 +0530

    added readme file

--- Bank_roulette-who_will_pay_the_bill.py
+++ Bank_roulette-who_will_pay_the_bill.py
@@ -1,5 +1,8 @@
 import random
 names_string = input("Give me everybody's names, seperated by a comma. ")
 names = names_string.split(", ")
-payer = random.randint(0,len(names))
-print(f"{names[payer]} will pay the bill.")
+# payer = random.randint(0,len(names) - 1)
+# print(f"{names[payer]} will pay the bill.")
+
+payer = random.choice(names)
+print(f"{payer} will pay the bill.")
