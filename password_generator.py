a99a7578ddbd77f00de399861cfa61500037de55
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-29 09:13:39 +0530

    added readme file

--- password_generator.py
+++ password_generator.py
@@ -7,7 +7,7 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 print("""---------------------------------------------------------------------------------
                     Welcome to the PyPassword Generator! 2022
 ---------------------------------------------------------------------------------""")
-nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
