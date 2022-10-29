dd71c5989f22eb57cfdf8e2108b759cbd8abc52b
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-29 09:24:40 +0530

    added readme file

--- password_generator.py
+++ password_generator.py
@@ -9,4 +9,4 @@ print("""-----------------------------------------------------------------------
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
+0
