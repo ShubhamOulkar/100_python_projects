f953e33e836daf620c6a546d67c79be3518c16ee
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-29 09:59:09 +0530

    added readme file

--- password_generator.py
+++ password_generator.py
@@ -9,4 +9,13 @@ print("""-----------------------------------------------------------------------
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-0
+
+words = random.sample(letters,nr_letters)
+numb = random.sample(numbers, nr_numbers)
+character = random.sample(symbols, nr_symbols)
+
+password = words + numb + character
+random.shuffle(password)
+print(f"Generated Password : {''.join(password)}")
+
+print("-------------------- oulkarshubhu@gmail.com -------------------")
