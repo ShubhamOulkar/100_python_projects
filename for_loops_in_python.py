fa7124f37da0ad47cd54c700b8108e8653dea262
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-29 08:58:49 +0530

    added readme file

---
+++ for_loops_in_python.py
@@ -0,0 +1,23 @@
+# How max() works ?
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
+# h = 0
+# for i in student_scores:
+#     if i > h:
+#         h = i
+# print(f"highest = {h}")
+
+# adding even numbers
+# sum = 0
+# for number in range(2,101,2):
+#     sum += number
+# print(sum)
+#
+# sum2 = 0
+# for number in range(0,101,2):
+#     if number % 2 == 0:
+#         sum2 += number
+# print(sum2)
+
