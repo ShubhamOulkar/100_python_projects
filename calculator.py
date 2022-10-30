a0cb60e215fe2181caeb86bcb923e6fdc792708d
shubham oulkar <oulkarshubhu@gmail.com>
2022-10-30 14:17:21 +0530

    added readme file

--- calculator.py
+++ calculator.py
@@ -18,10 +18,20 @@ operations = {
 "/":devide
 }
 
-a = int(input("Enter number: "))
-sign = input("Choose operations +, -, * or / : ")
-b = int(input("Enter number: "))
+keep_doing = True
 
-value = operations[sign]
-function_call = value(a,b)
-print(f"{a} {sign} {b} = {function_call}")
+while keep_doing :
+    a = int(input("Enter number: "))
+    sign = input("Choose operations +, -, * or / : ")
+    b = int(input("Enter number: "))
+
+    value = operations[sign]
+    function_call = value(a, b)
+    print(f"{a} {sign} {b} = {function_call}")
+
+    repe = input("Do you want to run calculator again? y/n ").lower()
+    if repe == "y":
+        keep_doing = True
+    else:
+        keep_doing = False
+
