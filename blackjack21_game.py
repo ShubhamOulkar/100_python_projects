6633b3fd8e803068e05f906ff152430ecb285f5c
shubham oulkar <oulkarshubhu@gmail.com>
2022-12-25 16:38:28 +0530

    added readme file

--- blackjack21_game.py
+++ blackjack21_game.py
@@ -1,18 +1,23 @@
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+card = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def ga():
-    cards = [random.randint(1, 11), random.randint(1, 11)]
-    gambler = [random.randint(1, 11)]
+    cards = [random.choice(card), random.choice(card)]
+    gambler = [random.choice(card), random.choice(card)]
     print(f"Your cards {cards}")
     print(f"Dealer card {gambler}")
-    extra = input("Do you want extra card? y/n")
+    extra = input("Do you want extra card? y/n ")
     if extra == "y":
-        cards.append(random.randint(1,11))
+        cards.append(random.choice(card))
         c = sum(cards)
-        gambler.append(random.randint(1,11))
-        g = sum(gambler)
+        g1 = sum(gambler)
+        if g1 < 17:
+            gambler.append(random.choice(card))
+            g = sum(gambler)
+        else:
+            g = sum(gambler)
+
         if c > 21 :
             print(f"your score:{c} {cards},\ndealer score : {g} {gambler},\n Dealer Win!")
         elif c > g :
@@ -21,17 +26,31 @@ def ga():
             print(f"your score:{c} {cards},\ndealer score : {g} {gambler}, \n Dealer Win!")
     else:
         c = sum(cards)
-        gambler.append(random.randint(1,11))
-        g = sum(gambler)
-        if c > 21 :
+        g1 = sum(gambler)
+        if g1 < 17:
+            gambler.append(random.choice(card))
+            g = sum(gambler)
+        else:
+            g = sum(gambler)
+
+        if c > 21 and g <= 21:
             print(f"your score:{c} {cards},\ndealer score : {g} {gambler},\n Dealer Win!")
-        elif c > g :
+        elif c > g and c <= 21 :
             print(f"your score:{c} {cards},\ndealer score : {g} {gambler},\n You Win!")
-        elif c < g :
+        elif c < g and g <= 21:
             print(f"your score:{c} {cards},\ndealer score : {g} {gambler}, \n Dealer Win!")
+        elif c == g :
+            print(f"your score:{c} {cards},\ndealer score : {g} {gambler}, \n Game Tied !")
+        elif c < g and g >= 21:
+            print(f"your score:{c} {cards},\ndealer score : {g} {gambler}, \n You Win!")
+
+
+
 
 condition = input("Do you want to play 21/blackjack game? y/n ").lower()
 
 if condition == "y":
     ga()
+else:
+    print("\n Thank you for visiting.")
 
