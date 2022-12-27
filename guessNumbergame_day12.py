5889295087d1dc880cb845cfa825d660d6fbbcf2
shubham oulkar <oulkarshubhu@gmail.com>
2022-12-27 18:56:15 +0530

    :fire:

--- guessNumbergame_day12.py
+++ guessNumbergame_day12.py
@@ -1,8 +1,7 @@
 import random
-from GuessGame_art import logo
 
 rn = random.randint(1,100)
-print(logo)
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 level = input("Choose a difficulty. Type 'easy' or 'hard': ")
@@ -16,18 +15,18 @@ def guess_game(diff):
     a = 1
     while a < diff:
         gn = int(input("Guess a number: "))
-        if gn != rn:
-            print(f"You have {diff - 1 - a} attempts remaining to guess the number.")
         a += 1
         if rn > gn:
-            print("Guess is TOO SMALL")
+            print("Guess is TOO SMALL.\nGuess Again.")
         elif rn < gn:
-            print("Guess is TOO LARGE")
+            print("Guess is TOO LARGE.\nGuess Again.")
         else:
             a = 12
-            print("Perfect Guess, you win ! ?")
+            print("Perfect Guess, you win ! ")
+        if gn != rn:
+            print(f"You have {diff - a} attempts remaining to guess the number.")
         if a == diff and gn != rn:
-            print("Your attempts are exhausted. \nYou lose ! ?")
+            print("Your attempts are exhausted.\nYou lose ! ")
 
 if level == "easy" or level == "hard":
     guess_game(diff)
