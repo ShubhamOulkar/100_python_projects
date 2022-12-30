7030b54008facd30485c182f6e305ca736a7920e
shubham oulkar <oulkarshubhu@gmail.com>
2022-12-30 11:51:53 +0530

    🎯

---
+++ Day 16 OOP in python/main.py
@@ -0,0 +1,19 @@
+# import turtle
+#
+# timmy = turtle.Turtle() # Turtle() is class
+# print(timmy)
+# timmy.shape('turtle') # method
+# timmy.color('red') # method
+# timmy.forward(100.00) # method
+#
+# my_screen = turtle.Screen() # Screen is class
+# print(my_screen.canvwidth) # canvwidth is attribute for class Screen
+# my_screen.exitonclick() # method of an object my_screen
+
+import prettytable
+
+table = prettytable.PrettyTable()
+table.add_column("Pokemon Name", ['Pikachu', 'shubha', 'shubha'])
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
+table.align = 'l'
+print(table)
