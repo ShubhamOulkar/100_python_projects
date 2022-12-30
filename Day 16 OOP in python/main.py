b906011134d762d526243378873b552d440c9e17
shubham oulkar <oulkarshubhu@gmail.com>
2022-12-30 14:50:52 +0530

    :man: PascalCase,🐪camelCase,:snake: Snake_case

--- Day 16 OOP in python/main.py
+++ Day 16 OOP in python/main.py
@@ -10,10 +10,11 @@
 # print(my_screen.canvwidth) # canvwidth is attribute for class Screen
 # my_screen.exitonclick() # method of an object my_screen
 
-import prettytable
+# import prettytable
+#
+# table = prettytable.PrettyTable()
+# table.add_column("Pokemon Name", ['Pikachu', 'shubha', 'shubha'])
+# table.add_column('Type', ['Electric', 'Water', 'Fire'])
+# table.align = 'l'
+# print(table)
 
-table = prettytable.PrettyTable()
-table.add_column("Pokemon Name", ['Pikachu', 'shubha', 'shubha'])
-table.add_column('Type', ['Electric', 'Water', 'Fire'])
-table.align = 'l'
-print(table)
