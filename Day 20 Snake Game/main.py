4de4bf34552c5437827ec5b88cba5ad6e52812a6
shubham oulkar <oulkarshubhu@gmail.com>
2023-01-02 16:47:29 +0530

    now snake moving

--- Day 20 Snake Game/main.py
+++ Day 20 Snake Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,7 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-#food = Food()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +22,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Food
+    if snake.head.distance(food) < 15:
+        print("colllision")
 
 screen.exitonclick()
