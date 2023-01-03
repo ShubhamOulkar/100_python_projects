22f28c2cd7a8ccb85d5cb073fd123cd4de61f738
shubham oulkar <oulkarshubhu@gmail.com>
2023-01-03 19:21:37 +0530

    snake game highest score update file

--- Day 20 Snake Game/scoreboard.py
+++ Day 20 Snake Game/scoreboard.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
+
+import self as self
+
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,6 +11,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('score.txt') as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,11 +20,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} Highest Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score >= self.high_score:
+            self.high_score = self.score
+            with open('score.txt', mode='w') as data:
+                data.write(str(self.high_score))
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
