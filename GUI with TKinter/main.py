48811647433320a022ce84ca223d928c54789013
shubham oulkar <oulkarshubhu@gmail.com>
2023-01-10 12:16:36 +0530

    GUI for Miles to kilometer converter

--- GUI with TKinter/main.py
+++ GUI with TKinter/main.py
@@ -3,12 +3,15 @@ from tkinter import Tk, Label, Button, Entry
 window = Tk()
 window.title("My 1st GUI")
 window.minsize(width=500, height= 300)
+window.config(padx= 100, pady= 100)
 
 # # label
 label = Label(text = "I'm a label.", font=("Arial",24,"bold"))
-label.pack()
-label['text'] = 'New Text'
 label.config(text= "New Text")
+#label.pack(side='left')
+label.place(x = 0, y = 0)
+label.config(padx= 100, pady= 100)
+
 
 # Button
 
@@ -18,20 +21,19 @@ def click():
     label.config(text= text)
 
 button = Button(text="Click me", command = click)
-button.pack()
+button.grid(column= 3, row= 2)
+button.config(padx= 2, pady= 2)
 
 # Enter Input
 input = Entry(width=10)
-input.pack()
 print(input.get())
+#input.place(x= 400, y= 250)
+input.grid(column=2, row=0)
 
 
-
-
-
-
-
-
-
-
+# new button
+new_button = Button(text="New Button", command = click)
+#new_button.place(x=300,y=50)
+new_button.grid(column=3, row=3)
+new_button.config(padx= 5, pady= 5)
 window.mainloop()
