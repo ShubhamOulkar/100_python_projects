f2e83be658994445a71b252b5851b7c8ec8edada
shubham oulkar <oulkarshubhu@gmail.com>
2023-01-12 12:12:57 +0530

    code modifications

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,16 +1,16 @@
 from tkinter import *
-from tkinter import  messagebox
+from tkinter import messagebox
 import random
 import json
 
 # UI setup
 window = Tk()
 window.title("My password Manager")
-window.config(padx= 50, pady= 50)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height= 200)
-img = PhotoImage(file = "logo.png")
-canvas.create_image(100,100, image = img)
+canvas = Canvas(width=200, height=200)
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=img)
 canvas.grid(column=1, row=0)
 
 
@@ -23,8 +23,8 @@ def save_data():
     user = Username_entry.get()
     pas = password_entry.get()
 
-    with open('password_manager.json','r') as file:
-        data =json.load(file)
+    with open('password_manager.json', 'r') as file:
+        data = json.load(file)
 
     if web in data:
         messagebox.showinfo(title='Website Exist', message='Already present in data base')
@@ -36,30 +36,29 @@ def save_data():
                         }
                      }
 
-
     if len(web) == 0 or len(user) == 0 or len(pas) == 0:
-        messagebox.askretrycancel(title= 'Empty Boxes', message= "Some fields are empty. \n Please check !")
+        messagebox.askretrycancel(title='Empty Boxes', message="Some fields are empty. \n Please check !")
     else:
-        try :
-            with open('password_manager.json', mode= 'r') as  file:
-                data = json.load(file) # loading old data
-        except FileNotFoundError :
+        try:
+            with open('password_manager.json', mode='r') as file:
+                data = json.load(file)  # loading old data
+        except FileNotFoundError:
             with open('password_manager.json', 'w') as file:
                 json.dump(table, file, indent=4)
         else:
-            data.update(table) # updating lod data with new data
+            data.update(table)  # updating lod data with new data
             with open('password_manager.json', 'w') as file:
-                json.dump(data, file, indent= 4) # saving updated data
-
+                json.dump(data, file, indent=4)  # saving updated data
 
             website_entry.delete(0, END)
             password_entry.delete(0, END)
             Username_entry.delete(0, END)
 
+
 def website_search():
     website = website_entry.get()
-    try :
-        with open("password_manager.json","r") as file :
+    try:
+        with open("password_manager.json", "r") as file:
             web_data = json.load(file)
     except FileNotFoundError:
         messagebox.showinfo(title='Error', message="No data file found.")
@@ -77,27 +76,27 @@ def website_search():
 website_label = Label(text='Website name:')
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width= 53)
+website_entry = Entry(width=53)
 website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
 
 Username_label = Label(text='Email/Username:')
 Username_label.grid(column=0, row=2)
 
-Username_entry = Entry(width= 53)
-Username_entry.grid(column=1, row=2, columnspan= 2)
-Username_entry.insert(0,"user@gmail.com")
+Username_entry = Entry(width=53)
+Username_entry.grid(column=1, row=2, columnspan=2)
+Username_entry.insert(0, "user@gmail.com")
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
 
 password_entry = Entry(width=53)
-password_entry.grid(column=1, row= 3)
+password_entry.grid(column=1, row=3)
 
 generatePassword_button = Button(text='Click for Password Generator', command=password_generator, width=45)
-generatePassword_button.grid(column= 1, row= 5)
+generatePassword_button.grid(column=1, row=5)
 
-add_button = Button(text='Add to a list', width= 45, command=save_data)
+add_button = Button(text='Add to a list', width=45, command=save_data)
 add_button.grid(column=1, row=6, columnspan=2)
 
 search_button = Button(text='Search Website', width=15, command=website_search)
@@ -134,12 +133,12 @@ def password_generate():
         num_numbers = int(q2_entry.get())
         num_char = int(q3_entry.get())
 
-        try :
+        try:
             words = random.sample(letters, num_letters)
             numb = random.sample(numbers, num_numbers)
             character = random.sample(symbols, num_char)
         except ValueError:
-            messagebox.showinfo(title='Value Error', message= "Input valid numbers.")
+            messagebox.showinfo(title='Value Error', message="Input valid numbers.")
             q1_entry.delete(0, END)
             q2_entry.delete(0, END)
             q3_entry.delete(0, END)
@@ -151,8 +150,7 @@ def password_generate():
             show_password.insert(0, "".join(password))
             password_entry.insert(0, "".join(password))
 
-
-    generate_password_button = Button(text="Generate password", command= generater)
+    generate_password_button = Button(text="Generate password", command=generater)
     generate_password_button.grid(column=1, row=12, columnspan=2)
 
     show_password = Entry(width=25)
