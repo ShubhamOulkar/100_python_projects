35aa43645db11bfbe3dc101c5d62d16abb190723
shubham oulkar <oulkarshubhu@gmail.com>
2023-02-23 12:12:41 +0530

    python code integration in html file

--- web development/back-end/web-visiting-card/server.py
+++ web development/back-end/web-visiting-card/server.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
 import datetime
 
+now_year = datetime.datetime.now().year
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def  home():
-    now_year = datetime.datetime.now().year
     return render_template('index.html', year=now_year)
+
+
+@app.route("/blogs")
+def get_blogs():
+    return render_template('blogs.html', year=now_year)
+
+
+@app.route("/blogs/post")
+def get_post():
+    return render_template('post.html', year=now_year)
 
 
 if __name__ == "__main__":
